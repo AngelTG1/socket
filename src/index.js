0fd5412a23b5a2822267115dee01d0b4ddcaf76c
AngelTG1 <angeltgz30@gmail.com>
2024-07-30 22:14:29 -0600

    tercer2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://n0k760zz-5174.usw3.devtunnels.ms/mapa"
+        origin: "*"
     }
 });
 
